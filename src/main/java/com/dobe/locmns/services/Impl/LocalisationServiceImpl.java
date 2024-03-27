c8fa814970ca362bd3ebd3a5507380ef57e9744c
ComeDobe <christiandobe@hotmail.fr>
2024-03-27 21:35:40 +0100

    mise à jour des classes

---
+++ src/main/java/com/dobe/locmns/services/Impl/LocalisationServiceImpl.java
@@ -0,0 +1,56 @@
+package com.dobe.locmns.services.Impl;
+
+import com.dobe.locmns.dto.LocalisationDto;
+import com.dobe.locmns.models.Localisation;
+import com.dobe.locmns.repositories.LocalisationRepository;
+import com.dobe.locmns.services.LocalisationService;
+import com.dobe.locmns.validators.ObjectsValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LocalisationServiceImpl implements LocalisationService {
+    private final LocalisationRepository localisationRepository;
+    private ObjectsValidator<LocalisationDto> validator;
+
+
+    @Override
+    public Integer save(LocalisationDto dto) {
+       validator.validate(dto);
+        Localisation localisation = LocalisationDto.toEntity(dto);
+        return localisationRepository.save(localisation).getId();
+    }
+
+    @Override
+    public Integer update(LocalisationDto dto) {
+        Localisation localisation = LocalisationDto.toEntity(dto);
+        return localisationRepository.save(localisation).getId();
+    }
+
+    @Override
+    public List<LocalisationDto> findAll() {
+        return  localisationRepository.findAll()
+                .stream()
+                .map(LocalisationDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public LocalisationDto findById(Integer id) {
+        return localisationRepository.findById(id)
+                .map(LocalisationDto::fromEntity)
+                .orElse(null);
+    }
+
+    @Override
+    public Void delete(Integer id) {
+        Localisation localisation = localisationRepository.findById(id)
+                .orElse(null);
+        localisationRepository.delete(localisation);
+        return null;
+    }
+}
---
+++ src/main/java/com/dobe/locmns/services/Impl/PannesServiceImpl.java
@@ -0,0 +1,55 @@
+package com.dobe.locmns.services.Impl;
+
+import com.dobe.locmns.dto.PannesDto;
+import com.dobe.locmns.models.Pannes;
+import com.dobe.locmns.repositories.PannesRepository;
+import com.dobe.locmns.services.PannesService;
+import com.dobe.locmns.validators.ObjectsValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PannesServiceImpl implements PannesService {
+    private final PannesRepository pannesRepository;
+    private ObjectsValidator<PannesDto> validator;
+
+    @Override
+    public Integer save(PannesDto dto) {
+        validator.validate(dto);
+        Pannes pannes = PannesDto.toEntity(dto);
+        return pannesRepository.save(pannes).getId();
+    }
+
+    @Override
+    public Integer update(PannesDto dto) {
+        Pannes pannes = PannesDto.toEntity(dto);
+        return pannesRepository.save(pannes).getId();
+    }
+
+    @Override
+    public List<PannesDto> findAll() {
+        return   pannesRepository.findAll()
+                .stream()
+                .map(PannesDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PannesDto findById(Integer id) {
+        return pannesRepository.findById(id)
+                .map(PannesDto::fromEntity)
+                .orElse(null);
+    }
+
+    @Override
+    public Void delete(Integer id) {
+        Pannes pannes = pannesRepository.findById(id)
+                .orElse(null);
+        pannesRepository.delete(pannes);
+        return null;
+    }
+}
