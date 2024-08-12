acc2aba6ec33d2f9047a6956d42ac1b5accfa8a5
ComeDobe <christiandobe@hotmail.fr>
2024-08-12 23:35:44 +0200

    mise à jour des classes

---
+++ src/main/java/com/dobe/locmns/controllers/MaterielController.java
@@ -0,0 +1,15 @@
+package com.dobe.locmns.controllers;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/materiel")
+@AllArgsConstructor
+public class MaterielController {
+
+
+
+}
