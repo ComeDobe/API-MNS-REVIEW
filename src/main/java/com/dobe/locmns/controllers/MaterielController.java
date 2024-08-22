b0857260901d0ccd5d5e0aa4e1613fae6289a508
ComeDobe <christiandobe@hotmail.fr>
2024-08-22 15:32:35 +0200

    mise à jour des classes

--- src/main/java/com/dobe/locmns/controllers/MaterielController.java
+++ src/main/java/com/dobe/locmns/controllers/MaterielController.java
@@ -1,15 +1,41 @@
 package com.dobe.locmns.controllers;
 
 
+import com.dobe.locmns.dto.MaterielDto;
+import com.dobe.locmns.services.MaterielService;
+import com.dobe.locmns.services.PretService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/materiel")
 @AllArgsConstructor
 public class MaterielController {
 
+    private final MaterielService materielService;
+    private final PretService pretService;
 
+    @PostMapping("")
+    public ResponseEntity<Integer> save(@RequestBody MaterielDto materielDto) {
+        return ResponseEntity.ok(materielService.save(materielDto));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<MaterielDto>> findAll() {
+        return ResponseEntity.ok(materielService.findAll());
+    }
+    @PutMapping("")
+    public ResponseEntity<Integer> update(@RequestBody MaterielDto materielDto) {
+        return ResponseEntity.ok(materielService.update(materielDto));
+
+    }
+    @GetMapping("/{materielId}")
+    public ResponseEntity<MaterielDto> findById(@PathVariable Integer materielId) {
+        return ResponseEntity.ok(materielService.findById(materielId));
+    }
 
 }
