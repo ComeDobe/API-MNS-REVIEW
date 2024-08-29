55f5701caee18609e3a40d5cb4278dae9226d2df
ComeDobe <christiandobe@hotmail.fr>
2024-08-30 01:58:03 +0200

    mise à jour des securités et de la config

--- src/main/java/com/dobe/locmns/controllers/MaterielController.java
+++ src/main/java/com/dobe/locmns/controllers/MaterielController.java
@@ -9,11 +9,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +25,6 @@ import java.util.List;
 public class MaterielController {
 
     private final MaterielService materielService;
-
     private final ReservationService reservationService;
     private final EmailService emailService;
     private final UserDetailsService userDetailsService;
@@ -32,18 +34,33 @@ public class MaterielController {
         return ResponseEntity.ok(materielService.save(materielDto));
     }
 
-    @GetMapping("")
-    public ResponseEntity<List<MaterielDto>> findAll() {
-        return ResponseEntity.ok(materielService.findAll());
-    }
+
     @PutMapping("")
     public ResponseEntity<Integer> update(@RequestBody MaterielDto materielDto) {
         return ResponseEntity.ok(materielService.update(materielDto));
 
     }
-    @GetMapping("/{materielId}")
-    public ResponseEntity<MaterielDto> findById(@PathVariable Integer materielId) {
-        return ResponseEntity.ok(materielService.findById(materielId));
+    @GetMapping({"", "/{materielId}"})
+    public ResponseEntity<?> getMateriel(@PathVariable(required = false) Integer materielId) {
+        try {
+            if (materielId != null) {
+                MaterielDto materiel = materielService.findById(materielId);
+                return ResponseEntity.ok(materiel);
+            } else {
+                List<MaterielDto> materiels = materielService.findAll();
+                return ResponseEntity.ok(materiels);
+            }
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur est survenue");
+        }
+    }
+
+    @DeleteMapping("/{materielId}")
+    public ResponseEntity<Void> delete(@PathVariable Integer materielId) {
+        materielService.delete(materielId);
+        return ResponseEntity.noContent().build();
     }
 
 }
